5bfe40775a392f85dedb9abbb47bd9dcd4a253c2
Aldcejam <aldcejamj923@gmail.com>
2022-04-26 11:34:02 -0300

    fi

--- src/pages/components/Regulation.tsx
+++ src/pages/components/Regulation.tsx
@@ -61,14 +61,14 @@ export function Regulation() {
                     <br />
                     <p>Caso tenha comprado seu pacote separado das pessoas que viajarão com você, o número do pedido de cada um deverá ser informado no formulário de agendamento dos outros integrantes para agilizar o processo de reserva de todos.</p>
                     <br />
-                    <p>Obs.: Nem sempre conseguimos garantir o mesmo voo e/ou hotel para todos os viajantes com pedidos vinculados. Vai depender do número de viajantes e da disponibilidade. Caso deseje que todos estejam no mesmo voo e/ou hotel, nos informe no campo  "Viajando com outras pessoas”, o número dos pedidos dos outros viajantes para verificarmos a disponibilidade.</p>
+                    <p>Obs.: Nem sempre conseguimos garantir o mesmo voo e/ou hotel para todos os viajantes com pedidos vinculados. Vai depender do número de viajantes e da disponibilidade. Caso deseje que todos estejam no mesmo voo e/ou hotel, nos informe no campo  Viajando com outras pessoas”, o número dos pedidos dos outros viajantes para verificarmos a disponibilidade.</p>
                     <br />
                     <h4>Pagamentos:</h4>
                     <p>Para finalizar a compra do seu pacote, você pode optar por pagar em até 12x no cartão de crédito ou no boleto bancário à vista. Se desejar pagar no boleto parcelado, sem consulta ao SPC e SERASA, você poderá consultar o número de parcelas disponíveis na página de pagamento, antes de finalizar a compra.  </p>
                     <br />
                     <p>Atenção para o período em que deseja viajar, pois o pagamento em boleto parcelado deverá ser concluído com, no mínimo, 60 dias de antecedência do período em que deseja efetivar a sua viagem.</p>
                     <br />
-                    <h4>Fique Ligado"(a)"!</h4>
+                    <h4>Fique Ligado!</h4>
                     <br />
                     <p>O não pagamento de algum boleto em até 10 dias após a compra resulta no cancelamento automático do pedido.</p>
                     <br />
