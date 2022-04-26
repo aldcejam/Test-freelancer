63bc542892fe45aa127d8d0129b153fafd2cb538
Aldcejam <aldcejamj923@gmail.com>
2022-04-26 15:31:36 -0300

    fi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -81,28 +81,28 @@ const Home: NextPage = () => {
         <CardRoteiroDoPacote
           withImage={true}
           withVideo={true}
-          image='/illustration/GrupoDeMascara.png'
+          image='/GrupoDeMascara.png'
           title='1º dia - Check-in em Bonito'
           text='Acomodação na hospedagem. Você ainda pode aproveitar para conhecer os passeios urbanos da cidade, como o Projeto Jiboia e o Aquário de Bonito, descansar e jantar em um dos restaurantes de Bonito (atividades não incluídas no pacote, contratar à parte). Ao final do dia, pernoite.'
         />
         <CardRoteiroDoPacote
           withImage={true}
           withVideo={false}
-          image='/illustration/imgDia2.png'
+          image='/imgDia2.png'
           title='2º dia Grutas de São Miguel + Estância Mimosa'
           text='Grutas de São Miguel: saída do hotel às 08h30. Pela manhã, um passeio repleto de surpresas em uma das principais grutas da cidade. Na sequência, visita e passeio nas Cachoeiras Estância Mimosa, com almoço incluso. Retorno e pernoite na hospedagem.'
         />
         <CardRoteiroDoPacote
           withImage={false}
           withVideo={true}
-          image='/illustration/imgDia3.png'
+          image='/imgDia3.png'
           title='3º dia Flutuação + Balneário Nascente Azul'
           text='Nascente Azul: saída do hotel às 08h30. O dia inteiro de atrações com o Combo Flutuação + Balneário, incluindo delicioso almoço! Retorno e pernoite na hospedagem.'
         />
         <CardRoteiroDoPacote
           withImage={false}
           withVideo={false}
-          image='/illustration/imgDia4.png'
+          image='/imgDia4.png'
           title='4º dia - Balneário do Sol'
           text='Saída do hotel às 08h30. O dia inteiro relaxando em meio às lindas paisagens do Rio Formoso + variedade de lazer para todas as idades. Check-out na hospedagem após o passeio.'
         />
@@ -163,7 +163,7 @@ const Home: NextPage = () => {
       <section className=' bg-white pb-[15vh]'>
         <div className='flex justify-center max-w-[1250px] mx-auto xl:mx-10 pt-16 px-10 mt-14 rounded-2xl'>
           <div className='w-[45%] '>
-            <img className='w-[100%]' src="/Illustration/paradises.png" alt="" />
+            <img className='w-[100%]' src="/paradises.png" alt="" />
           </div>
 
           <div className='w-[60%] py-10 pl-20 '>
