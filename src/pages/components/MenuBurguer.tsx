43d8f4b5760c5cbbd85c22998357d3fd29f4be89
Aldcejam <aldcejamj923@gmail.com>
2022-05-03 07:46:13 -0300

    final

--- src/pages/components/MenuBurguer.tsx
+++ src/pages/components/MenuBurguer.tsx
@@ -3,14 +3,15 @@ import Link from "next/link"
 type MenuBurguer = {
     isActive: boolean
 }
+const styleBotton = 'flex items-center opacity-0 w-[60vw] h-[50px] pl-4 py-2 cursor-pointer border-b-[1px] border-grayBorder'
 export default function MenuBurguer({ isActive }: MenuBurguer) {
     return (
-        <div className={`${isActive ? 'animationMenuBurguer' : null} bg-white absolute right-0 top-12 text-lg pt-2 rounded-md`} >
+        <div className={`${isActive ? 'animationMenuBurguer' : null} bg-white absolute right-0 top-12 text-lg rounded-md`} >
             <Link href={'/'}>
-                <div className={`${isActive ? 'block itenMenuBurguer' : 'none'} flex items-center opacity-0 w-[200px] h-[50px] pl-4 cursor-pointer`}>OPÇÃO1</div>
+                <div className={`${isActive ? 'block itenMenuBurguer' : 'none'} ${styleBotton} `}>OPÇÃO1</div>
             </Link>
             <Link href={'/'}>
-                <div className={`${isActive ? 'block itenMenuBurguer' : 'none'} flex items-center opacity-0 w-[200px] h-[50px] pl-4 cursor-pointer`}>OPÇÃO2</div>
+                <div className={`${isActive ? 'block itenMenuBurguer' : 'none'} ${styleBotton}`}>OPÇÃO2</div>
             </Link>
         </div>
     )
