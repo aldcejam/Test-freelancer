13570ffb308b49841ee3fe156b2a6269baad73f9
Aldcejam <aldcejamj923@gmail.com>
2022-05-07 15:43:29 -0300

    final

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,9 +8,12 @@ export default class MyDocument extends Document {
                 <Head>
                     <link rel="shortcut icon" href="favicon.png" type="image/png" />
                     {/* =============== Font Family =============== */}
-                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap" rel="stylesheet"></link>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"></link>
                     <title>Hotel</title>
                 </Head>
                 <body>
